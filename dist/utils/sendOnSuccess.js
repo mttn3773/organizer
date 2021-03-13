fd03fb8e8e6c0673d1132e81bba8c702312fbd93
mttn373 <mttn3773@gmail.com>
2021-03-13 13:18:32 +0400

    deleted dist

--- dist/utils/sendOnSuccess.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendOnSuccess = void 0;
-const sendOnSuccess = ({ res, msg }, props) => {
-    return res.json(Object.assign({ success: true, msg }, props)).end();
-};
-exports.sendOnSuccess = sendOnSuccess;
-//# sourceMappingURL=sendOnSuccess.js.map
